1c9df0966347b5eb8bd4b3947bfa4786adb8c75a
ppitest <vinicius_guitar_007@hotmail.com>
2016-10-06 21:15:52 -0300

    Sql Banco-PF

---
+++ sql_PPI_PF.sql
@@ -0,0 +1,42 @@
+﻿
+CREATE TABLE public.usuario (
+                login VARCHAR(20) NOT NULL,
+                senha VARCHAR(50) NOT NULL,
+                nome VARCHAR(80) NOT NULL,
+                categoria CHAR(1) NOT NULL,
+                situao CHAR(1) NOT NULL,
+                CONSTRAINT usuario_pk PRIMARY KEY (login)
+);
+COMMENT ON COLUMN public.usuario.senha IS 'criptografar em md5';
+COMMENT ON COLUMN public.usuario.nome IS 'nome completo';
+COMMENT ON COLUMN public.usuario.situao IS '(A)ativo/(I)inativo';
+
+
+CREATE TABLE public.aluno (
+                matricula CHAR(15) NOT NULL,
+                nome VARCHAR(80) NOT NULL,
+                sexo CHAR(1) NOT NULL,
+                dtnasc DATE NOT NULL,
+                cidade VARCHAR(40) NOT NULL,
+                uf CHAR(2) NOT NULL,
+                CONSTRAINT aluno_pk PRIMARY KEY (matricula)
+);
+COMMENT ON COLUMN public.aluno.sexo IS 'M/F';
+COMMENT ON COLUMN public.aluno.cidade IS 'cidade de nascimento';
+COMMENT ON COLUMN public.aluno.uf IS 'sigla do estado em que nasceu';
+
+
+CREATE TABLE public.disciplina (
+                codigo INT NOT NULL,
+                nome VARCHAR(80) NOT NULL,
+                ch VARCHAR(80) NOT NULL
+);
+COMMENT ON COLUMN public.disciplina.ch IS 'Carga Horários';
+
+
+CREATE TABLE public.curso (
+                numero INT NOT NULL,
+                nome VARCHAR(150) NOT NULL,
+                sigla CHAR(3) NOT NULL,
+                CONSTRAINT curso_pk PRIMARY KEY (numero)
+);
