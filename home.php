8c1428e8892c39c1396f4979f31e78e80a7abecf
Diego <diegomr7@gmail.com>
2016-10-09 20:25:31 -0300

    Atualização do menu

--- home.php
+++ home.php
@@ -4,45 +4,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="imgs/formatura.png">
+     <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="shortcut icon" href="imgs/formatura.png">
+ 
+     <title>Final Project</title>
 
-    <title>Final Project</title>
-
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+     <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
 
-    <div class="container">
+     <div class="container">
           <div class="header clearfix">
-              <nav>
-                  <ul class="nav nav-pills pull-right">
-                      <li role="presentation" class="active"><a href="home.php">Home</a></li>
-                      <li role="presentation"><a href="cadUser.php">Cadastrar usuário</a></li>
-                      <li role="presentation"><a href="logout.php">Logout</a></li>
-                  </ul>
-              </nav>
-              <h3 class="text-muted">Final Project</h3>
+               <nav>
+                    <ul class="nav nav-pills pull-right">
+                         <li role="presentation" class="active"><a href="home.php">Home</a></li>
+                         <li role="presentation">
+                              <a href="#" data-toggle="dropdown" class="dropdown-toggle">Cadastro</a>
+                              <ul class="dropdown-menu">
+                                   <li><a href="#">Aluno</a></li>
+                                   <li><a href="cadUser.php">Usuário</a></li>
+                              </ul>
+                         </li>
+                         <li role="presentation"><a href="logout.php">Logout</a></li>
+                    </ul>
+               </nav>
+               <h3 class="text-muted">Final Project</h3>
           </div>
   
           <div class="jumbotron">
-              <!-- Aqui agente fala alguma coisa sobre o trabalho no geral -->
+               <!-- Aqui agente fala alguma coisa sobre o trabalho no geral -->
           </div>
   
           <div class="row marketing">
-              <div class="col-lg-6">
-                  <!-- Aqui agente fala coisas específicas sobre uma parte do trabalho para dar destaque -->
-              </div>
+               <div class="col-lg-6">
+                    <!-- Aqui agente fala coisas específicas sobre uma parte do trabalho para dar destaque -->
+               </div>
           </div>
   
           <footer class="footer">
-              <p>&copy; 2016 Dav, Inc.</p>
+               <p>&copy; 2016 Dav, Inc.</p>
           </footer>
 
-    </div>
+     </div>
+     
+     <script src="js/jquery.min.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/funcoes.js"></script>
 </body>
 </html>
